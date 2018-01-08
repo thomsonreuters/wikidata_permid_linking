e3ebc172df8237923cb73949d0ebc4d4970ea28c
Dan Bennett <dan.bennett@thomsonreuters.com>
2018-01-08 09:38:41 -0600

    Fallback to search if no hit from match

--- organizations/without_leis/match.py
+++ organizations/without_leis/match.py
@@ -22,12 +22,33 @@ from __future__ import print_function
 
 
 import sys
+import locale
+import codecs
 import requests
 import time
 from SPARQLWrapper import SPARQLWrapper, JSON
 
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
+
+def search_by_company_name(access_token, name):
+	headers = {'X-AG-Access-Token' : access_token}
+	url = 'https://api.thomsonreuters.com/permid/search?q='+name+'&entitytype=organization'
+	ret = None
+	try:
+		response = requests.get(url, headers=headers)
+	except Exception as e:
+		print ('Error in connect ' , e)
+		return
+	if response.status_code == 200:
+		j_response = response.json()
+		org_count = j_response['result']['organizations']['total']
+		if org_count > 0:
+			orgs =  j_response['result']['organizations']['entities']
+			ret = orgs
+	else:
+		raise Exception(u"Invalid response from permid.org {0}".format(response))
+	return ret
 
 def permid_match_api(access_token, payload):
 	headers = {'X-AG-Access-Token' : access_token,
@@ -37,8 +58,6 @@ def permid_match_api(access_token, payload):
 	url = 'https://api.thomsonreuters.com/permid/match'
 	ret = None
 	try:
-		#eprint('request')
-		eprint(payload.encode('utf-8'))
 		response = requests.post(url, data=payload.encode('utf-8'), headers=headers)
 	except Exception  as e:
 		eprint (u'Error in connect ' , e)
@@ -50,6 +69,9 @@ def permid_match_api(access_token, payload):
 		raise Exception("Invalid response from permid.org {0}".format(response))
 	return ret
 
+# from https://stackoverflow.com/questions/4545661/unicodedecodeerror-when-redirecting-to-file
+# Wrap sys.stdout into a StreamWriter to allow writing unicode.
+sys.stdout = codecs.getwriter(locale.getpreferredencoding())(sys.stdout)
 
 if len(sys.argv) < 2:
 	print ('Get companies without LEIs from wikidata, match to orgs on permid.org')
@@ -70,20 +92,21 @@ SELECT ?item ?itemLabel ?country ?countryLabel ?url ?headquarters_location ?head
   MINUS { ?item wdt:P3347 ?permID. }
   OPTIONAL { ?item wdt:p856 ?url. }
   ?item wdt:P17 ?country.
-  SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "en,de". }
   ?item wdt:P159 ?headquarters_location.
 }
-LIMIT 40
+LIMIT 50
 }
 ORDER BY ?itemLabel""")
 
 sparql.setReturnFormat(JSON)
 results = sparql.query().convert()
 count = 0
+
 str_list = []
 str_list.append(u'LocalID,Name,Country,City,Website')
 previous_org_code = ""
-# for each lei, search permid.org by LEI for the organization
+
 for result in results["results"]["bindings"]:
 	url, sep, code = result["item"]["value"].rpartition('/')
 	if previous_org_code == code:
@@ -93,7 +116,6 @@ for result in results["results"]["bindings"]:
 	company_url = u""
 	if result.has_key("url"):
 		company_url = result["url"]["value"]
-
 	str_list.append(u"{0},\"{1}\",\"{2}\",\"{3}\",{4}".format(code,
 		result["itemLabel"]["value"],
 		result["countryLabel"]["value"],
@@ -104,8 +126,20 @@ if matches is not None:
 	for match in matches:
 		if match["Match Level"] == "Excellent":
 			url,sep, permID = match["Match OpenPermID"].rpartition('-')
-			print(u"{0}\tP3347\t\"{1}\"\t{2}"
-				.format(match["Input_LocalID"],permID,match["Match OrgName"]))
+			print(u"{0}\tP3347\t\"{1}\"\t{2}={3}"
+				.format(match["Input_LocalID"],permID,match["Input_Name"],match["Match OrgName"]))
 		else:
-			eprint(u"No match for {0}, {1}"
+			eprint(u"No match for {0}, '{1}', trying search by name"
 				.format(match["Input_Name"],match["Input_LocalID"]))
+			found_orgs = search_by_company_name(access_token,match["Input_Name"])
+			time.sleep(.5)
+			if found_orgs is None:
+				eprint(u"No search match for org {0}".format(match["Input_Name"]))
+			else:
+				for org in found_orgs:
+					eprint(org)
+					org_name = org['organizationName']
+					uri, separator, permid = org['@id'].rpartition('-')
+					print(u"{0}\tP3347\t\"{1}\"\t{2}\t{3}={4}"
+					.format(match["Input_LocalID"],permid,count,match["Input_Name"],			org_name))
+		count = count + 1
